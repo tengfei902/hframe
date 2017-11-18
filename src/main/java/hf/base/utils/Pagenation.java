2cbc6f928300f869eea1c88a151cf5ba3b739c24
tengfei <tengfei@tengfeideMacBook-Pro.local>
2017-11-18 23:08:29 +0800

    add

---
+++ src/main/java/hf/base/utils/Pagenation.java
@@ -0,0 +1,41 @@
+package hf.base.utils;
+
+import com.google.common.collect.Lists;
+import hf.base.contants.Constants;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pagenation<T> {
+    private int currentPage;
+    private int nextPage;
+    private int pageSize;
+    private int totalPage;
+    private int totalSize;
+    private List<T> data;
+
+    public Pagenation(List<T> list) {
+
+    }
+
+    public Pagenation(List<T> list,int currentPage,int pageSize) {
+        if(CollectionUtils.isEmpty(list)) {
+            this.currentPage = 1;
+            this.nextPage = 1;
+            this.pageSize = pageSize;
+            this.totalPage = 1;
+            this.totalSize = 0;
+            this.data = new ArrayList<>();
+        } else {
+            List<List<T>> lists = Lists.partition(list,pageSize);
+            this.totalSize = list.size();
+            this.pageSize = pageSize;
+            this.totalPage = lists.size();
+            int actualPage = lists.size();
+            this.currentPage = actualPage>currentPage?currentPage:actualPage;
+            this.nextPage = this.currentPage+1>actualPage?actualPage:this.currentPage+1;
+            this.data = lists.get(this.currentPage);
+        }
+    }
+}
