2135d880b63ebbaf54b45b1e5a11387d414039ef
tengfei <tengfei@tengfeideMacBook-Pro.local>
2017-11-25 12:21:39 +0800

    add

---
+++ src/main/java/hf/base/enums/OprStatus.java
@@ -0,0 +1,19 @@
+package hf.base.enums;
+
+public enum OprStatus {
+    NEW(0),
+    PAY_SUCCESS(1),
+    PAY_FAILED(99),
+    FINISHED(10),
+    REFUND(98);
+
+    private int value;
+
+    OprStatus(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
