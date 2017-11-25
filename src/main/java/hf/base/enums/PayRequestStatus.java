cc1cdd121ec102ee75b2e3e4b6310884c1099ec0
tengfei <tengfei@tengfeideMacBook-Pro.local>
2017-11-25 17:55:20 +0800

    add

--- src/main/java/hf/base/enums/PayRequestStatus.java
+++ src/main/java/hf/base/enums/PayRequestStatus.java
@@ -1,22 +1,28 @@
 package hf.base.enums;
 
 public enum PayRequestStatus {
-    NEW(0),
-    OPR_GENERATED(1),
-    REMOTE_CALL_FINISHED(2),
-    PAY_SUCCESS(5),
-    OPR_SUCCESS(10),
-    PAY_FAILED(98),
-    OPR_FINISHED(99);
+    NEW(0,"未处理"),
+    OPR_GENERATED(1,"处理中"),
+    REMOTE_CALL_FINISHED(2,"成功,未返回"),
+    PAY_SUCCESS(5,"成功，已返回"),
+    OPR_SUCCESS(10,"交易成功"),
+    PAY_FAILED(98,"交易失败"),
+    OPR_FINISHED(99,"交易失败");
 
     private int value;
+    private String desc;
 
-    PayRequestStatus(int value) {
+    PayRequestStatus(int value,String desc) {
         this.value = value;
+        this.desc = desc;
     }
 
     public int getValue() {
         return value;
+    }
+
+    public String getDesc() {
+        return desc;
     }
 
     public static PayRequestStatus parse(int value) {
