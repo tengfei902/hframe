8a2bdc715833ed0ace068040c1cdddd2f2783656
tengfei <tengfei@tengfeideMacBook-Pro.local>
2017-11-26 22:12:31 +0800

    add

---
+++ src/main/java/hf/base/enums/SettleStatus.java
@@ -0,0 +1,21 @@
+package hf.base.enums;
+
+public enum SettleStatus {
+    NEW(0,"未处理"),PROCESSING(1,"处理中"),SUCCESS(10,"处理完成"),FAILED(99,"处理失败");
+
+    private int value;
+    private String desc;
+
+    SettleStatus(int value,String desc) {
+        this.value = value;
+        this.desc = desc;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
