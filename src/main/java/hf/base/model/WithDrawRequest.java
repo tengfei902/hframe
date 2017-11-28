2ad538a57061d428e07b68333a5b8d5271f8d38b
tengfei <tengfei@tengfeideMacBook-Pro.local>
2017-11-29 00:44:13 +0800

    add

--- src/main/java/hf/base/model/WithDrawRequest.java
+++ src/main/java/hf/base/model/WithDrawRequest.java
@@ -1,4 +1,56 @@
 package hf.base.model;
 
+import hf.base.annotations.Field;
+
 public class WithDrawRequest {
+    @Field(required = true)
+    private Long groupId;
+    @Field
+    private String mchId;
+    @Field
+    private Integer status;
+    @Field(required = true,defaults = "1")
+    private Integer currentPage;
+    @Field(required = true,defaults = "15")
+    private Integer pageSize;
+
+    public Long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Long groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getMchId() {
+        return mchId;
+    }
+
+    public void setMchId(String mchId) {
+        this.mchId = mchId;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(Integer currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
 }
