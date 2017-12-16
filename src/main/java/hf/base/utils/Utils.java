aea0fb37c699c53fc37aa91061537355b68cdaca
tengfei <tengfei@tengfeideMacBook-Pro.local>
2017-12-17 00:08:03 +0800

    add

--- src/main/java/hf/base/utils/Utils.java
+++ src/main/java/hf/base/utils/Utils.java
@@ -134,4 +134,15 @@ public class Utils {
         str = str.append("key=").append(cipherCode);
         return DigestUtils.md5Hex(str.toString()).toUpperCase();
     }
+
+    public static String getRandomString(int length) {
+        String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(base.length());
+            sb.append(base.charAt(number));
+        }
+        return sb.toString();
+    }
 }
