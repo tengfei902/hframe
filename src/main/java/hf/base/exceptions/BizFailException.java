c1d18bdb8ded16008f3695964bcc163e240155a9
tengfei <tengfei@tengfeideMacBook-Pro.local>
2017-12-17 20:54:52 +0800

    add

--- src/main/java/hf/base/exceptions/BizFailException.java
+++ src/main/java/hf/base/exceptions/BizFailException.java
@@ -2,15 +2,21 @@ package hf.base.exceptions;
 
 public class BizFailException extends RuntimeException {
 
+    private String code;
+    private String message;
+
     public BizFailException() {
     }
 
     public BizFailException(String message) {
         super(message);
+        this.message = message;
     }
 
     public BizFailException(String code,String message) {
         super(String.format("%s:%s",code,message));
+        this.code = code;
+        this.message = message;
     }
 
     public BizFailException(String message, Throwable cause) {
