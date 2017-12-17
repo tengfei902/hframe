d1cce00cacd71d3ab72e4f4f4c89375d308e9241
tengfei <tengfei@tengfeideMacBook-Pro.local>
2017-12-17 17:44:46 +0800

    add

--- src/main/java/hf/base/enums/OperateType.java
+++ src/main/java/hf/base/enums/OperateType.java
@@ -1,7 +1,12 @@
 package hf.base.enums;
 
 public enum OperateType {
-    IN(0),OUT(1);
+    USER_HF(0),
+    HF_CLIENT(1),
+    CLIENT_HF(2),
+    HF_USER(3),
+    CALLBACK_CLIENT_HF(4),
+    CALLBACK_HF_USER(5);
 
     private int value;
 
