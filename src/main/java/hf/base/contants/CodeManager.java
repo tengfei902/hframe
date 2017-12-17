bc68bc4c2aa748e5ee5dadf8ec5790134cea9d64
tengfei <tengfei@tengfeideMacBook-Pro.local>
2017-12-17 20:43:41 +0800

    add

--- src/main/java/hf/base/contants/CodeManager.java
+++ src/main/java/hf/base/contants/CodeManager.java
@@ -6,4 +6,10 @@ public class CodeManager {
     public static final String RESET_PASSWORD_FAILED = "9000002";
     public static final String GET_USER_SUCCESS = "0000000";
     public static final String BIZ_FAIELD = "9999999";
+
+    public static final String PAY_SUCCESS = "0";
+    public static final String CHECK_ENCRIPT_FAILED = "1";
+    public static final String PARAM_CHECK_FAILED = "2";
+    public static final String PAY_FAILED = "3";
+    public static final String WAITING = "4";
 }
