4cb3a3cfbd883a6a6f3c98d509c49f2a2380a584
tengfei <tengfei@tengfeideMacBook-Pro.local>
2017-12-17 22:25:38 +0800

    add

--- src/main/java/hf/base/enums/ChannelCode.java
+++ src/main/java/hf/base/enums/ChannelCode.java
@@ -4,7 +4,15 @@ public enum ChannelCode {
     WX_OPEN("pay.weixin.jspay"),
     ALI("pay.alipay.jspay"),
     QQ("pay.tenpay.native"),
-    WX("pay.weixin.raw.app");
+    WX("pay.weixin.raw.app"),
+    FXT_WX_OPEN_ID("01"),
+    FXT_WX("02"),
+    FXT_WX_FS("03"),
+    FXT_WX_H5("04"),
+    FXT_WX_APP("05"),
+    FXT_ALI("06"),
+    FXT_ALI_ZS("07"),
+    FXT_ALI_FS("08");
 
     private String service;
 
