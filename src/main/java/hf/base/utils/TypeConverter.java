112079e38d078d29c88c7751b121866a62a00ae5
tengfei <tengfei@tengfeideMacBook-Pro.local>
2018-01-04 15:36:13 +0800

    add

--- src/main/java/hf/base/utils/TypeConverter.java
+++ src/main/java/hf/base/utils/TypeConverter.java
@@ -23,58 +23,62 @@ public class TypeConverter {
 
     private static Map<Class,Map<String,PropertyDescriptor>> beanCache = new ConcurrentHashMap<>();
 
-    public static <T> T convert(Map<String,Object> request,Class<T> dataType) throws Exception {
-        T data = dataType.newInstance();
-        BeanInfo beanInfo = Introspector.getBeanInfo(dataType);
+    public static <T> T convert(Map<String,Object> request,Class<T> dataType) {
+        try {
+            T data = dataType.newInstance();
+            BeanInfo beanInfo = Introspector.getBeanInfo(dataType);
 
-        if(MapUtils.isEmpty(beanCache.get(dataType))) {
-            PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+            if(MapUtils.isEmpty(beanCache.get(dataType))) {
+                PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
 
-            Map<String,PropertyDescriptor> map = new HashMap<>();
-            for(PropertyDescriptor propertyDescriptor : propertyDescriptors) {
-                map.put(propertyDescriptor.getName(),propertyDescriptor);
-            }
-            beanCache.put(dataType,map);
-        }
-
-        Field[] fields = dataType.getDeclaredFields();
-
-        for(Field field:fields) {
-            if(!field.isAnnotationPresent(hf.base.annotations.Field.class)) {
-                continue;
-            }
-
-            hf.base.annotations.Field fieldValue = field.getDeclaredAnnotation(hf.base.annotations.Field.class);
-
-            String fieldStr = String.valueOf(request.get(StringUtils.isEmpty(fieldValue.alias())?field.getName():fieldValue.alias()));
-            String value = (StringUtils.isEmpty(fieldStr) || org.apache.commons.lang3.StringUtils.equalsIgnoreCase("null",fieldStr)) ?fieldValue.defaults():fieldStr;
-
-            if(org.apache.commons.lang3.StringUtils.isEmpty(value) || org.apache.commons.lang3.StringUtils.equalsIgnoreCase("null",value)) {
-                if(fieldValue.required()) {
-                    throw new BizFailException("field empty");
+                Map<String,PropertyDescriptor> map = new HashMap<>();
+                for(PropertyDescriptor propertyDescriptor : propertyDescriptors) {
+                    map.put(propertyDescriptor.getName(),propertyDescriptor);
                 }
-                continue;
+                beanCache.put(dataType,map);
             }
 
-            if(fieldValue.required() && org.apache.commons.lang3.StringUtils.isEmpty(value)) {
-                throw new BizFailException(String.format("%s cannot be empty",field.getName()));
-            } else if(Long.class.isAssignableFrom(field.getType())) {
-                beanCache.get(dataType).get(field.getName()).getWriteMethod().invoke(data,new BigDecimal(value).longValue());
-            }else if(Integer.class.isAssignableFrom(field.getType())) {
-                beanCache.get(dataType).get(field.getName()).getWriteMethod().invoke(data,new BigDecimal(value).intValue());
-            }else if(Date.class.isAssignableFrom(field.getType())) {
-                LocalDateTime localDateTime = LocalDateTime.parse(value);
-                ZoneId zone = ZoneId.systemDefault();
-                Instant instant = localDateTime.atZone(zone).toInstant();
-                Date date = Date.from(instant);
-                beanCache.get(dataType).get(field.getName()).getWriteMethod().invoke(data,date);
-            } else if(BigDecimal.class.isAssignableFrom(field.getType())){
-                beanCache.get(dataType).get(field.getName()).getWriteMethod().invoke(data,new BigDecimal(value));
-            } else {
-                beanCache.get(dataType).get(field.getName()).getWriteMethod().invoke(data,value);
+            Field[] fields = dataType.getDeclaredFields();
+
+            for(Field field:fields) {
+                if(!field.isAnnotationPresent(hf.base.annotations.Field.class)) {
+                    continue;
+                }
+
+                hf.base.annotations.Field fieldValue = field.getDeclaredAnnotation(hf.base.annotations.Field.class);
+
+                String fieldStr = String.valueOf(request.get(StringUtils.isEmpty(fieldValue.alias())?field.getName():fieldValue.alias()));
+                String value = (StringUtils.isEmpty(fieldStr) || org.apache.commons.lang3.StringUtils.equalsIgnoreCase("null",fieldStr)) ?fieldValue.defaults():fieldStr;
+
+                if(org.apache.commons.lang3.StringUtils.isEmpty(value) || org.apache.commons.lang3.StringUtils.equalsIgnoreCase("null",value)) {
+                    if(fieldValue.required()) {
+                        throw new BizFailException("field empty");
+                    }
+                    continue;
+                }
+
+                if(fieldValue.required() && org.apache.commons.lang3.StringUtils.isEmpty(value)) {
+                    throw new BizFailException(String.format("%s cannot be empty",field.getName()));
+                } else if(Long.class.isAssignableFrom(field.getType())) {
+                    beanCache.get(dataType).get(field.getName()).getWriteMethod().invoke(data,new BigDecimal(value).longValue());
+                }else if(Integer.class.isAssignableFrom(field.getType())) {
+                    beanCache.get(dataType).get(field.getName()).getWriteMethod().invoke(data,new BigDecimal(value).intValue());
+                }else if(Date.class.isAssignableFrom(field.getType())) {
+                    LocalDateTime localDateTime = LocalDateTime.parse(value);
+                    ZoneId zone = ZoneId.systemDefault();
+                    Instant instant = localDateTime.atZone(zone).toInstant();
+                    Date date = Date.from(instant);
+                    beanCache.get(dataType).get(field.getName()).getWriteMethod().invoke(data,date);
+                } else if(BigDecimal.class.isAssignableFrom(field.getType())){
+                    beanCache.get(dataType).get(field.getName()).getWriteMethod().invoke(data,new BigDecimal(value));
+                } else {
+                    beanCache.get(dataType).get(field.getName()).getWriteMethod().invoke(data,value);
+                }
             }
+
+            return data;
+        } catch (Exception e) {
+            throw new BizFailException(e.getMessage());
         }
-
-        return data;
     }
 }
